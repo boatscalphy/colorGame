3d0c2f679f864829f4b93aedfc2e045b24cc8ca5
Calvin La <calvinhangla.dev@gmail.com>
2019-04-09 13:12:13 -0700

    JS - adding functionality for EASY/HARD buttons to change difficulty of the game

--- colorgame.js
+++ colorgame.js
@@ -1,4 +1,4 @@
-// Function to generate a random integer
+// Function to generate a random integer exclusive of the max specified
 function randomInt(max) {
     return Math.floor(Math.random() * (max));
 }
@@ -7,7 +7,7 @@ function randomInt(max) {
 function rngSquares(square, colors) {
 
     for (i=0; i < square.length; i++) {
-        square[i].style.backgroundColor=colors[i]
+        square[i].style.backgroundColor = colors[i]
     }
 }
 
@@ -16,7 +16,15 @@ function generateColor() {
     var red = String(randomInt(255));
     var green = String(randomInt(255));
     var blue = String(randomInt(255));
-    return "rgb(" + red + ", " + green + ", " + blue + ")"
+
+    //Making sure we do not randomly generate the same color as the board.
+    if (red === 50 && green === 50 && blue === 50) {
+        generateColor();
+    }
+    
+    else { 
+        return "rgb(" + red + ", " + green + ", " + blue + ")"
+    }
 }
 
 //Function to create 6 random RGB colors to assign to squares in rngSquares function
@@ -36,6 +44,8 @@ function assignSquare() {
     return arr;
 }
 
+//Creating a variablbe to indicate what the current game mode is - default is HARD.
+var gameMode = "HARD"
 //Squares in game have a class of sqare. Using this variable to store them for use in rngSquares/ event listening.
 var squares = document.querySelectorAll(".square")
 
@@ -52,37 +62,82 @@ rngSquares(squares, colorList)
 //Choosing a random square's rgb value for the game.
 rgbText.innerText = colorList[randomInt(6)]
 
+//Variable to store the banner properties which will be used to access background color (.sytle.backgroundColor)
 var banner = document.querySelector(".banner")
+
+//Creating an event listener to generate a new list of colors for tiles when clicked on.
+document.querySelector(".newcolors").addEventListener("click",function(){
+    banner.style.backgroundColor = "rgb(78, 119, 206)"
+    colorList = assignSquare();
+    rngSquares(squares, colorList);
+    rgbText.innerText = colorList[randomInt(6)];
+})
 
 //Adding event listeners for each square
 for (i=0; i < squares.length; i++) {
     squares[i].addEventListener("click",function(){
-
-        //if the selected square is the correct RGB value display Corect! and Reset the board with a new set of colors
-        if (this.style.backgroundColor.toUpperCase() === rgbText.innerText) {
-            resp.innerText = "Correct!";
-
-            setTimeout(function(){
-                resp.innerText = null;
-            }, 750)
-
-            colorList = assignSquare();
-            banner.style.backgroundColor = rgbText.innerText;
-            rngSquares(squares, colorList);
-            rgbText.innerText = colorList[randomInt(6)];
-
+        
+        //Only allow selection of tiles if they are not the same color as the background
+        if (this.style.backgroundColor.toUpperCase() !== "RGB(50, 50, 50)") {
+            //if the selected square is the correct RGB value display Corect! and Reset the board with a new set of colors
+            if (this.style.backgroundColor.toUpperCase() === rgbText.innerText) {
+                resp.innerText = "Correct!";
+    
+                setTimeout(function(){
+                    resp.innerText = null;
+                }, 1750)
+                
+                colorList = assignSquare();
+                banner.style.backgroundColor = rgbText.innerText;
+                rngSquares(squares, colorList);
+                rgbText.innerText = colorList[randomInt(6)];
+           
+            }
+                
+            //if the selected square does not match the RGB text in the banner - notify the user that that was an incorrect selection and hide the square.
+            else {
+                this.style.backgroundColor = "rgb(50, 50, 50)";
+                resp.innerText = "Wrong!";
+    
+                setTimeout(function(){
+                    resp.innerText = null;
+                }, 1750);
+    
+            }
+            
         }
-
-        //if the selected square does not match the RGB text in the banner - notify the user that that was an incorrect selection and hide the square.
-        else {
-            this.style.backgroundColor = "rgb(50, 50, 50)";
-            resp.innerText = "Wrong!";
-
-            setTimeout(function(){
-                resp.innerText = null;
-            }, 750);
-
-        }
-
     })
+        
 }
+
+//Creating Event Listeners for the difficulty button
+var difficulty = document.querySelectorAll(".difficulty")
+
+for (i=0; i < difficulty.length; i++) {
+
+    difficulty[i].addEventListener("click",function(){
+        //Display 3 tiles for Easy mode
+        if (this.innerText === "EASY") {
+            gameMode = "EASY"        
+            colorList = assignSquare()
+            rngSquares(squares, colorList)
+            rgbText.innerText = colorList[randomInt(4)]
+            console.log(randomInt(6))
+            for (j=3; j < squares.length; j++) {
+                squares[j].style.backgroundColor = "RGB(50, 50, 50)"
+            }
+        }
+
+        //Display all 6 tiles for hard mode
+        else {
+            gameMode = "HARD"
+            colorList = assignSquare()
+            rngSquares(squares, colorList)
+            rgbText.innerText = colorList[randomInt(6)]
+        }
+            
+    })
+
+}
+
+
